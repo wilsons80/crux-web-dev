fb302cc630ea119eb2ddf1ac58fe4725e44f59a6
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-08-13 19:28:54 -0300

    colacando logo e rota nova senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TrocarSenhaComponent } from './components/trocar-senha/trocar-senha.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -7,6 +8,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'trocarsenha', component: TrocarSenhaComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   { path: '', redirectTo: '/home', pathMatch:'full'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './components/login/login.module';
 import { HomeModule } from './components/home/home.module';
 import { HttpClientModule } from '@angular/common/http';
+import { TrocarSenhaComponent } from './components/trocar-senha/trocar-senha.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrocarSenhaComponent
   ],
   imports: [
     BrowserModule,
