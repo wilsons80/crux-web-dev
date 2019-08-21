83d70ac7fc695202bd9c0d0349028eeb0ff0a69b
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-08-20 22:18:09 -0300

    rotas acessos

--- src/app/components/acesso/acesso-routing.module.ts
+++ src/app/components/acesso/acesso-routing.module.ts
@@ -1,11 +1,17 @@
+import { AlterarAcessoComponent } from './alterar-acesso/alterar-acesso.component';
+import { ConsultarAcessoComponent } from './consultar-acesso/consultar-acesso.component';
 import { CadastrarAcessoComponent } from './cadastrar-acesso/cadastrar-acesso.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from 'src/app/guards/auth.guard';
+import { DeletarAcessoComponent } from './deletar-acesso/deletar-acesso.component';
 
 
 const routes: Routes = [
   { path: 'acesso/:idUnidade/cadastrar', component: CadastrarAcessoComponent},
+  { path: 'acesso/:idUnidade/consultar', component: ConsultarAcessoComponent},
+  { path: 'acesso/:idUnidade/deletar', component: DeletarAcessoComponent},
+  { path: 'acesso/:idUnidade/alterar', component: AlterarAcessoComponent},
   //{ path: 'acesso/:idUnidade/cadastrar', component: CadastrarAcessoComponent, canActivate: [AuthGuard]},
 ];
 
