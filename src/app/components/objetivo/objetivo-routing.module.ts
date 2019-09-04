c68cc443d25fd78ba28a2e62fe741e17d3bc307f
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-09-04 01:45:02 -0300

    routing

--- src/app/components/objetivo/objetivo-routing.module.ts
+++ src/app/components/objetivo/objetivo-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ObjetivoComponent } from './objetivo.component';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'objetivo', component: ObjetivoComponent,canActivate: [AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
