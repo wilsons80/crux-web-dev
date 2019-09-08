d4e0c7a3aa4475698e278fbdafe0dfe0286aef95
Wilson Souza <wilsons80@gmail.com>
2019-09-08 11:34:21 -0300

    Ajustes configuraçao heroku

--- proxy.config.js
+++ proxy.config.js
@@ -1,5 +1,10 @@
+
+//Capturando a porta do Heroku
+const PORTA = process.env.PORT || 8080;
+
 //TODO rever essa linha em produção
-const targetDomain = 'http://localhost:8080';
+const targetDomain = `http://localhost:${PORTA}`;
+
 //const targetDomain = process.env.PROXY_TARGET_DOMAIN ? `https://${process.env.PROXY_TARGET_DOMAIN}.mpdft.mp.br` : 'http://localhost:8080';
 console.log(`Usando o endereço "${targetDomain}" para o proxy...\n`);
 
