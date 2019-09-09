ee46c608180fd40c710ca81c13422e786e95408a
nrjosue <josue.rodrigues@mpdft.mp.br>
2019-09-09 19:19:16 -0300

    metas

--- src/app/components/metas/metas.module.ts
+++ src/app/components/metas/metas.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MetasRoutingModule } from './metas-routing.module';
 import { MetasComponent } from './metas.component';
-import { MatButtonModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, MatTableModule, MatListModule, MatDatepickerModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, MatTableModule, MatListModule, MatDatepickerModule, MatPaginatorModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { TextMaskModule } from 'angular2-text-mask';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -29,7 +29,8 @@ import { CadastrarMetasComponent } from './cadastrar-metas/cadastrar-metas.compo
     MatListModule,
     FlexLayoutModule,
     SharedPipesModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatPaginatorModule
   ]
 })
 export class MetasModule { }
