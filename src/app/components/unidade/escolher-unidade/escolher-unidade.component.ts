97dbb9b9bbd710bda81c9ebc8e50152edb3cb73d
nrjosue <josue.rodrigues@mpdft.mp.br>
2019-09-13 13:43:13 -0300

    ajuste escolha

--- src/app/components/unidade/escolher-unidade/escolher-unidade.component.ts
+++ src/app/components/unidade/escolher-unidade/escolher-unidade.component.ts
@@ -1,11 +1,12 @@
 import { MenuService } from './../../../services/menu/menu.service';
 import { Menu } from 'src/app/core/menu';
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { UnidadeService } from 'src/app/services/unidade/unidade.service';
 import { Unidade } from 'src/app/core/unidade';
 import { switchMap } from 'rxjs/operators';
 import { AcessoUnidade } from 'src/app/core/acesso-unidade';
+import { ToolbarPrincipalService } from 'src/app/services/toolbarPrincipal/toolbar-principal.service';
 
 @Component({
   selector: 'app-escolher-unidade',
@@ -13,30 +14,39 @@ import { AcessoUnidade } from 'src/app/core/acesso-unidade';
   styleUrls: ['./escolher-unidade.component.css']
 })
 export class EscolherUnidadeComponent implements OnInit {
-
+ 
+  
+  
+  
   unidades: AcessoUnidade[];
-
+  
   constructor(
     private unidadeService:UnidadeService,
     private menuService:MenuService,
-    private router:Router
-  ) { }
+    private router:Router,
+    private toolbarPrincipalService:ToolbarPrincipalService,
+    private changeDetectorRef: ChangeDetectorRef
+    ) { }
 
-  ngOnInit() {
-   this.unidadeService.getUnidadesComAcesso().subscribe((acessoUnidade: AcessoUnidade[]) => {
-     this.unidades = acessoUnidade;
-   })
+    ngOnInit() {
+    // this.unidades = this.toolbarPrincipalService.unidades;
+    console.log("unidade", this.unidades)
   }
-
+  
   escolherUnidade(idUnidade:number){
-
+    
     this.unidadeService.getUnidadePorId(idUnidade).pipe(
       switchMap((unidade:Unidade) => {return this.menuService.getMenuPrincipal()} )
-    )
+      )
     .subscribe((menu) => {
       console.log("menuzao da massa", menu);
       this.router.navigate([`home`]);
     })
+  }
+  
+  ngAfterContentChecked(): void {
+    this.unidades = this.toolbarPrincipalService.unidades;
+    this.changeDetectorRef.detectChanges();
   }
      
 }
