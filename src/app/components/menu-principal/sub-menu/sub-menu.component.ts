a813d948eda5c5e8d52c4d31dd77120779da3c53
nrjosue <josue.rodrigues@mpdft.mp.br>
2019-09-24 14:19:46 -0300

    tooltip

--- src/app/components/menu-principal/sub-menu/sub-menu.component.ts
+++ src/app/components/menu-principal/sub-menu/sub-menu.component.ts
@@ -12,6 +12,7 @@ export class SubMenuComponent implements OnInit {
   @Input() rota: string
   @Input() icone: string
   @Input() titulo: string
+  @Input() toolTip: string
 
   constructor(
     public menuPrincipalService: MenuPrincipalService,
