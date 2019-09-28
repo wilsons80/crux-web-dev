e946693b29b9e327797db92a6b2bafae6defb852
Wilson Souza <wilsons80@gmail.com>
2019-09-28 15:47:48 -0300

    Implementaçao do service Reprovacao Aluno e Atividade Aluno

--- src/app/services/atividade-aluno/atividade-aluno.service.ts
+++ src/app/services/atividade-aluno/atividade-aluno.service.ts
@@ -1,19 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BaseService } from '../base/base.service';
-import { ReprovacaoAluno } from 'src/app/core/reprovacao-aluno';
+import { AtividadeAluno } from 'src/app/core/atividade-aluno';
 
 interface Path {
-  rootPath: 'api/reprovacaoalino/';
+  rootPath: 'api/atividadesaluno/';
 }
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReprovacaoAlunoService extends BaseService<ReprovacaoAluno, Path> {
+export class AtividadeAlunoService extends BaseService<AtividadeAluno, Path> {
 
   constructor(http: HttpClient) {
     super(http);
   }
-  
 }
