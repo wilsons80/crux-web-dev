b01321e7b40836b1fad52f0e71dfdd302a08c62f
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-09-29 16:01:42 -0300

    talentos

--- src/app/components/faltas-funcionario/cadastrar-faltas-funcionario/cadastrar-faltas-funcionario.component.ts
+++ src/app/components/faltas-funcionario/cadastrar-faltas-funcionario/cadastrar-faltas-funcionario.component.ts
@@ -1,5 +1,13 @@
+import { Location } from '@angular/common';
+import { FaltasFuncionario } from 'src/app/core/faltas-funcionario';
 import { Component, OnInit } from '@angular/core';
-
+import { Funcionario } from 'src/app/core/funcionario';
+import { FuncionarioService } from 'src/app/services/funcionario/funcionario.service';
+import { FaltasFuncionarioService } from 'src/app/services/faltas-funcionario/faltas-funcionario.service';
+import { ActivatedRoute } from '@angular/router';
+import { ToastService } from 'src/app/services/toast/toast.service';
+import { Objetivo } from 'src/app/core/objetivo';
+import { Indicadores } from 'src/app/core/indicadores';
 @Component({
   selector: 'app-cadastrar-faltas-funcionario',
   templateUrl: './cadastrar-faltas-funcionario.component.html',
@@ -7,9 +15,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarFaltasFuncionarioComponent implements OnInit {
 
-  constructor() { }
+  funcionarios: Funcionario[];
+  faltasFuncionario: FaltasFuncionario = new FaltasFuncionario();
+
+  isAtualizar: boolean = false;
+
+  constructor(
+    private funcionarioService: FuncionarioService,
+    private faltasFuncionarioService: FaltasFuncionarioService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private toastService:ToastService
+  ) {
+  }
+
 
   ngOnInit() {
+    this.funcionarioService.getAll().subscribe((funcionarios: Funcionario[]) => {
+      this.funcionarios = funcionarios;
+    });
+
+    let idFaltaFuncionario: number;
+    idFaltaFuncionario = this.route.snapshot.queryParams.idFaltaFuncionario ? this.route.snapshot.queryParams.idFaltaFuncionario : null;
+    if (idFaltaFuncionario) {
+      this.isAtualizar = true;
+      this.faltasFuncionarioService.getById(idFaltaFuncionario).subscribe((faltasFuncionario: FaltasFuncionario) => {
+        this.faltasFuncionario = faltasFuncionario;
+      });
+    }
+
+  }
+  cadastrar() {
+    this.faltasFuncionarioService.cadastrar(this.faltasFuncionario).subscribe(() => {
+      this.location.back();
+      this.toastService.showSucesso("Falta cadastrada com sucesso");
+    });
+  }
+
+  limpar() {
+    this.faltasFuncionario = new FaltasFuncionario()
+  }
+
+  cancelar() {
+    this.location.back();
+  }
+
+  getNomeBotao() {
+    return this.isAtualizar ? 'Atualizar' : 'Cadastrar';
+  }
+
+  atualizar() {
+    this.faltasFuncionarioService.alterar(this.faltasFuncionario).subscribe(() => {
+      this.location.back();
+      this.toastService.showSucesso("Falta atualizada com sucesso");
+    });
+
   }
 
 }
--- src/app/components/talento/cadastrar-talento/cadastrar-talento.component.ts
+++ src/app/components/talento/cadastrar-talento/cadastrar-talento.component.ts
@@ -1,4 +1,15 @@
+import { PessoaFisica } from './../../../core/pessoa-fisica';
+import { Location } from '@angular/common';
+import { Talento } from 'src/app/core/talento';
 import { Component, OnInit } from '@angular/core';
+import { Funcionario } from 'src/app/core/funcionario';
+import { FuncionarioService } from 'src/app/services/funcionario/funcionario.service';
+import { FaltasFuncionarioService } from 'src/app/services/faltas-funcionario/faltas-funcionario.service';
+import { ActivatedRoute } from '@angular/router';
+import { ToastService } from 'src/app/services/toast/toast.service';
+import { FaltasFuncionario } from 'src/app/core/faltas-funcionario';
+import { TalentosService } from 'src/app/services/talentos/talentos.service';
+import { PessoaFisicaService } from 'src/app/services/pessoa-fisica/pessoa-fisica.service';
 
 @Component({
   selector: 'app-cadastrar-talento',
@@ -7,9 +18,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarTalentoComponent implements OnInit {
 
-  constructor() { }
+  funcionarios: Funcionario[];
+  pessoas: PessoaFisica[];
+  talento: Talento = new Talento();
+
+  isAtualizar: boolean = false;
+
+  constructor(
+    private funcionarioService: FuncionarioService,
+    private talentosService: TalentosService,
+    private pessoaFisicaService: PessoaFisicaService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private toastService:ToastService
+  ) { }
 
   ngOnInit() {
+    this.funcionarioService.getAll().subscribe((funcionarios: Funcionario[]) => {
+      this.funcionarios = funcionarios;
+    });
+    
+    this.pessoaFisicaService.getAll().subscribe((pessoas: PessoaFisica[]) => {
+      this.pessoas = pessoas;
+    });
+
+    let idFaltaFuncionario: number;
+    idFaltaFuncionario = this.route.snapshot.queryParams.idFaltaFuncionario ? this.route.snapshot.queryParams.idFaltaFuncionario : null;
+    if (idFaltaFuncionario) {
+      this.isAtualizar = true;
+      this.talentosService.getById(idFaltaFuncionario).subscribe((talento: Talento) => {
+        this.talento = talento;
+      });
+    }
+
   }
+  cadastrar() {
+    this.talentosService.cadastrar(this.talento).subscribe(() => {
+      this.location.back();
+      this.toastService.showSucesso("Talento cadastrado com sucesso");
+    });
+  }
+
+  limpar() {
+    this.talento = new Talento();
+  }
+
+  cancelar() {
+    this.location.back();
+  }
+
+  getNomeBotao() {
+    return this.isAtualizar ? 'Atualizar' : 'Cadastrar';
+  }
+
+  atualizar() {
+    this.talentosService.alterar(this.talento).subscribe(() => {
+      this.location.back();
+      this.toastService.showSucesso("Talento atualizado com sucesso");
+    });
+
+  }
+
+
 
 }
