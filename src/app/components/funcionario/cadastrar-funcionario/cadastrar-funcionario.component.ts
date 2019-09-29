1febf8e977b6ec29e5d0a63a9d1d6de3a9dc44b0
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-09-29 20:29:35 -0300

    ajustando

--- src/app/components/funcionario/cadastrar-funcionario/cadastrar-funcionario.component.ts
+++ src/app/components/funcionario/cadastrar-funcionario/cadastrar-funcionario.component.ts
@@ -4,6 +4,7 @@ import { ToastService } from 'src/app/services/toast/toast.service';
 import { Funcionario } from './../../../core/funcionario';
 import { PessoaFisica } from './../../../core/pessoa-fisica';
 import { FuncionarioService } from './../../../services/funcionario/funcionario.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-funcionario',
@@ -15,9 +16,11 @@ export class CadastrarFuncionarioComponent implements OnInit {
   pessoaFisica: PessoaFisica = new PessoaFisica();
   funcionario: Funcionario = new Funcionario();
 
+  isAtualizar: boolean = false;
 
   constructor(
     private funcionarioService: FuncionarioService,
+    private route: ActivatedRoute,
     private location: Location,
     private toastService: ToastService
   ) {
@@ -26,6 +29,15 @@ export class CadastrarFuncionarioComponent implements OnInit {
 
   ngOnInit() {
     this.funcionario.pessoasFisica = this.pessoaFisica;
+
+    let idFuncionario: number;
+    idFuncionario = this.route.snapshot.queryParams.idFuncionario ? this.route.snapshot.queryParams.idFuncionario : null;
+    if (idFuncionario) {
+      this.isAtualizar = true;
+      this.funcionarioService.getById(idFuncionario).subscribe((funcionario: Funcionario) => {
+        this.funcionario = funcionario;
+      });
+    }
   }
 
   cadastrar() {
@@ -39,9 +51,28 @@ export class CadastrarFuncionarioComponent implements OnInit {
   tratarDados() {
     this.funcionario.pessoasFisica.cep = this.funcionario.pessoasFisica.cep ? this.retiraMascara(this.funcionario.pessoasFisica.cep.toString()) : null
     this.funcionario.pessoasFisica.celular = this.funcionario.pessoasFisica.celular ? this.retiraMascara(this.funcionario.pessoasFisica.celular.toString()) : null
-    this.funcionario.pessoasFisica.cpf = this.funcionario.pessoasFisica.cpf ? this.retiraMascara(this.funcionario.pessoasFisica.cpf) : null
+    this.funcionario.pessoasFisica.cpf = this.funcionario.pessoasFisica.cpf ? this.retiraMascara(this.funcionario.pessoasFisica.cpf.toString()) : null
   }
 
+  limpar() {
+    this.funcionario = new Funcionario()
+  }
+
+  cancelar() {
+    this.location.back();
+  }
+
+  getNomeBotao() {
+    return this.isAtualizar ? 'Atualizar' : 'Cadastrar';
+  }
+
+  atualizar() {
+    this.funcionarioService.alterar(this.funcionario).subscribe(() => {
+      this.location.back();
+      this.toastService.showSucesso("Funcionário atualizado com sucesso");
+    });
+
+  }
 
   retiraMascara(objeto) {
     return objeto.replace(/\D/g, '');
