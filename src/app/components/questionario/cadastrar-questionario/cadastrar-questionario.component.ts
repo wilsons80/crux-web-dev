9aec37cd5695f1118d1a77c6b95329705d86108a
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-09-29 14:58:11 -0300

    cadastrar questionario

--- src/app/components/questionario/cadastrar-questionario/cadastrar-questionario.component.ts
+++ src/app/components/questionario/cadastrar-questionario/cadastrar-questionario.component.ts
@@ -1,4 +1,11 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Questionario } from 'src/app/core/questionario';
+import { QuestionarioService } from 'src/app/services/questionario/questionario.service';
+import { ObjetivoService } from 'src/app/services/objetivo/objetivo.service';
+import { ActivatedRoute } from '@angular/router';
+import { ToastService } from 'src/app/services/toast/toast.service';
+import { Objetivo } from 'src/app/core/objetivo';
 
 @Component({
   selector: 'app-cadastrar-questionario',
@@ -7,9 +14,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarQuestionarioComponent implements OnInit {
 
-  constructor() { }
+  questionario: Questionario = new Questionario();
+
+  isAtualizar: boolean = false;
+
+  tiposQuestionario = [
+    {id: 1, tipo: "T", descricao: "TALENTO" },
+    {id: 2, tipo: "G", descricao: "GRUPO FAMILIAR" },
+    {id: 3, tipo: "O", descricao: "OUTRO" },
+  ]
+  constructor(
+    private questionarioService: QuestionarioService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private toastService:ToastService
+  ) {
+  }
+
 
   ngOnInit() {
+
+    let idQuestionario: number;
+    idQuestionario = this.route.snapshot.queryParams.idQuestionário ? this.route.snapshot.queryParams.idQuestionário : null;
+    if (idQuestionario) {
+      this.isAtualizar = true;
+      this.questionarioService.getById(idQuestionario).subscribe((ind: Questionario) => {
+        this.questionario = ind;
+      });
+    }
+
+  }
+  cadastrar() {
+    this.questionarioService.cadastrar(this.questionario).subscribe(() => {
+      this.location.back();
+      this.toastService.showSucesso("Questionário cadastrado com sucesso");
+    });
+  }
+
+  limpar() {
+    this.questionario = new Questionario()
+  }
+
+  cancelar() {
+    this.location.back();
+  }
+
+  getNomeBotao() {
+    return this.isAtualizar ? 'Atualizar' : 'Cadastrar';
+  }
+
+  atualizar() {
+    this.questionarioService.alterar(this.questionario).subscribe(() => {
+      this.location.back();
+      this.toastService.showSucesso("Questionário atualizado com sucesso");
+    });
+
   }
 
 }
