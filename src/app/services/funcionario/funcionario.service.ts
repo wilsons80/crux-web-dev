20c2fc1e6ab97d62080f410b413c5a73b3d55d8a
Wilson Souza <wilsons80@gmail.com>
2019-09-30 18:49:25 -0300

    Ajustes no funcionario service para usar o BaseService

--- src/app/services/funcionario/funcionario.service.ts
+++ src/app/services/funcionario/funcionario.service.ts
@@ -1,36 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Rotas } from 'src/app/core/rotas';
+import { BaseService } from './../../../../.history/src/app/services/base/base.service_20190930175311';
 import { Funcionario } from './../../core/funcionario';
 
-
-const rootPath = 'api/funcionario/';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FuncionarioService {
-
-
-  constructor(private http: HttpClient) { }
-
-  getAll() {
-    return this.http.get(rootPath);
-  }
-
-  getById(id: number) {
-    return this.http.get(rootPath + `${id}`);
-  }
-
-  cadastrar(funcionario: Funcionario) {
-    return this.http.post(rootPath, funcionario);
-  }
-
-  alterar(funcionario: Funcionario) {
-    return this.http.put(rootPath, funcionario);
-  }
-
-  excluir(id: number) {
-    return this.http.delete(rootPath + `${id}`);
+export class FuncionarioService extends BaseService<Funcionario> {
+  constructor(http: HttpClient) {
+    super(http, Rotas.FUNCIONARIOS);
   }
 }
 
