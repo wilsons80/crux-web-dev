2757cbc5097a82522febfd40565f2af1eca9fff2
nrjosue <nrjosue@gmail.com>
2019-10-06 15:39:24 -0300

    documento atividade

--- src/app/components/documento-atividade/cadastrar-documento-atividade/cadastrar-documento-atividade.component.ts
+++ src/app/components/documento-atividade/cadastrar-documento-atividade/cadastrar-documento-atividade.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DocumentoAtividade } from "src/app/core/documento-atividade";
+import { Atividade } from "src/app/core/atividade";
+import { AtividadeService } from "src/app/services/atividade/atividade.service";
+import { DocumentoAtividadeService } from "src/app/services/documento-atividade/documento-atividade.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastService } from "src/app/services/toast/toast.service";
 
 @Component({
-  selector: 'app-cadastrar-documento-atividade',
-  templateUrl: './cadastrar-documento-atividade.component.html',
-  styleUrls: ['./cadastrar-documento-atividade.component.css']
+  selector: "app-cadastrar-documento-atividade",
+  templateUrl: "./cadastrar-documento-atividade.component.html",
+  styleUrls: ["./cadastrar-documento-atividade.component.css"]
 })
 export class CadastrarDocumentoAtividadeComponent implements OnInit {
+  documentoAtividade: DocumentoAtividade = new DocumentoAtividade();
+  atividades: Atividade[];
 
-  constructor() { }
+  isAtualizar: boolean = false;
+
+  constructor(
+    private atividadeService: AtividadeService,
+    private documentoAtividadeService: DocumentoAtividadeService,
+    private activitedRoute: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit() {
+
+    this.documentoAtividade.atividade = new Atividade();
+
+    this.atividadeService.getAll().subscribe((atividades: Atividade[]) => {
+      this.atividades = atividades;
+    });
+
+    let idDocumentoAtividade: number;
+    idDocumentoAtividade = this.activitedRoute.snapshot.queryParams
+      .idDocumentoAtividade
+      ? this.activitedRoute.snapshot.queryParams.idDocumentoAtividade
+      : null;
+    if (idDocumentoAtividade) {
+      this.isAtualizar = true;
+      this.documentoAtividadeService
+        .getById(idDocumentoAtividade)
+        .subscribe((documentoAtividade: DocumentoAtividade) => {
+          this.documentoAtividade = documentoAtividade;
+        });
+    }
+  }
+  cadastrar() {
+    this.documentoAtividadeService
+      .cadastrar(this.documentoAtividade)
+      .subscribe(() => {
+        this.router.navigate(["documentoatividade"]);
+        this.toastService.showSucesso(
+          "Documento Atividade cadastrado com sucesso"
+        );
+      });
   }
 
+  limpar() {
+    this.documentoAtividade = new DocumentoAtividade();
+  }
+
+  cancelar() {
+    this.router.navigate(["documentoatividade"]);
+  }
+
+  getNomeBotao() {
+    return this.isAtualizar ? "Atualizar" : "Cadastrar";
+  }
+
+  atualizar() {
+    this.documentoAtividadeService
+      .alterar(this.documentoAtividade)
+      .subscribe(() => {
+        this.router.navigate(["documentoatividade"]);
+        this.toastService.showSucesso(
+          "Documento Atividade cadastrado com sucesso"
+        );
+      });
+  }
 }
