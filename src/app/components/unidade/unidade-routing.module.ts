bac7109c59718a6a53bdffbe35e5ecbfa8d904cf
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-10-09 20:36:28 -0300

    nao foto

--- src/app/components/unidade/unidade-routing.module.ts
+++ src/app/components/unidade/unidade-routing.module.ts
@@ -5,12 +5,14 @@ import { CadastrarUnidadeComponent } from './cadastrar-unidade/cadastrar-unidade
 import { EscolherUnidadeComponent } from './escolher-unidade/escolher-unidade.component';
 import { UnidadeComponent } from './unidade.component';
 import { UnidadeResolver } from 'src/app/guards/unidades.resolve';
+import { AcessoModuloResolver } from 'src/app/guards/acesso-modulo.resolve';
+import { Modulos } from 'src/app/core/modulos';
 
 
 const routes: Routes = [
   { path: 'unidade/escolher', component: EscolherUnidadeComponent, canActivate: [AuthGuard], resolve: { unidades: UnidadeResolver } },
   { path: 'unidade/cadastrar', component: CadastrarUnidadeComponent, canActivate: [AuthGuard] },
-  { path: 'unidade', component: UnidadeComponent, canActivate: [AuthGuard] },
+  { path: 'unidade', component: UnidadeComponent, canActivate: [AuthGuard], resolve: {perfilAcesso:AcessoModuloResolver}, data:{modulo:Modulos.UNIDADE} },
 ];
 
 @NgModule({
