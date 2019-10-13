fab7c1dae9f9e8bef699ae4d1023c90bc49c4af0
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-10-13 13:02:02 -0300

    ajsuter na acao competencia

--- src/app/components/acao-competencia/acao-competencia.component.ts
+++ src/app/components/acao-competencia/acao-competencia.component.ts
@@ -1,12 +1,10 @@
-import { PerfilAcesso } from './../../core/perfil-acesso';
-import { PessoaFisicaService } from 'src/app/services/pessoa-fisica/pessoa-fisica.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatPaginator, MatTableDataSource } from '@angular/material';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AcaoCompetencia } from 'src/app/core/acao-competencia';
 import { AcoesCompetenciaService } from 'src/app/services/acoes-competencia/acoes-competencia.service';
 import { ConfirmDialogComponent } from '../common/confirm-dialog/confirm-dialog.component';
-import { PessoaFisica } from 'src/app/core/pessoa-fisica';
+import { PerfilAcesso } from './../../core/perfil-acesso';
 
 @Component({
   selector: 'app-acao-competencia',
@@ -21,57 +19,53 @@ export class AcaoCompetenciaComponent implements OnInit {
   mostrarTabela: boolean = false;
   acaoCompetencia: AcaoCompetencia = new AcaoCompetencia();
   msg: string;
-  pessoas:PessoaFisica[];
-  pessoa:PessoaFisica;
-  perfilAcesso:PerfilAcesso
 
+  perfilAcesso: PerfilAcesso;
 
   displayedColumns: string[] = ['descricao', 'dataInicio', 'acoes'];
+
+
   dataSource: MatTableDataSource<AcaoCompetencia> = new MatTableDataSource();
 
   constructor(
-    private acaoCompetenciaService: AcoesCompetenciaService,
-    private pessoaFisicaService:PessoaFisicaService,
+    private acoesCompetenciaService: AcoesCompetenciaService,
     private router: Router,
     private dialog: MatDialog,
-    private activatedRoute:ActivatedRoute
+    private activatedRoute: ActivatedRoute
+
   ) { }
 
   ngOnInit() {
-    this.perfilAcesso =  this.activatedRoute.snapshot.data.perfilAcesso[0];
+    this.perfilAcesso = this.activatedRoute.snapshot.data.perfilAcesso[0];
 
-    if(this.perfilAcesso.altera === 'N' && this.perfilAcesso.deleta === 'N'){
-      this.displayedColumns =['descricao', 'dataInicio'];
+    if (this.perfilAcesso.altera === 'N' && this.perfilAcesso.deleta === 'N') {
+      this.displayedColumns = ['descricao', 'dataInicio'];
     }
     this.dataSource.paginator = this.paginator;
-    this.pessoaFisicaService.getAll().subscribe((pessoas:PessoaFisica[]) => {
-      this.pessoas = pessoas;
-    })
+    this.getAll();
   }
 
 
   limpar() {
     this.mostrarTabela = false;
+    this.acaoCompetencia = new AcaoCompetencia()
     this.dataSource.data = [];
-    this.acaoCompetencia = new AcaoCompetencia();
-    this.msg = '';
   }
 
   consultar() {
-    this.acaoCompetenciaService.getPorPessoa(this.pessoa.id).subscribe((acaoCompetencia: AcaoCompetencia) => {
-      if(!acaoCompetencia){
-        this.mostrarTabela = false
-        this.msg = "Nenhum registro para a pesquisa selecionada"
-      }else {
-        this.dataSource.data = [acaoCompetencia];
-        this.mostrarTabela = true;
-      }
-    },
-    ()  => {
-      this.msg = "Nenhum registro para a pesquisa selecionada"
-      this.limpar()
+    if (this.acaoCompetencia.id) {
+      this.acoesCompetenciaService.getById(this.acaoCompetencia.id).subscribe((acaoCompetencia: AcaoCompetencia) => {
+        if (!acaoCompetencia) {
+          this.mostrarTabela = false
+          this.msg = "Nenhum registro para a pesquisa selecionada"
+        } else {
+          this.dataSource.data = [acaoCompetencia];
+          this.mostrarTabela = true;
+        }
+      })
+    } else {
+      this.getAll();
     }
-    )
   }
 
 
@@ -86,7 +80,7 @@ export class AcaoCompetenciaComponent implements OnInit {
   chamaCaixaDialogo(acaoCompetencia: AcaoCompetencia) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.data = {
-      pergunta: `Certeza que desse excluir a Ação Competência?`,
+      pergunta: `Certeza que desse excluir a ação competência?`,
       textoConfirma: 'SIM',
       textoCancela: 'NÃO'
     };
@@ -94,8 +88,8 @@ export class AcaoCompetenciaComponent implements OnInit {
     const dialogRef = this.dialog.open(ConfirmDialogComponent, dialogConfig);
     dialogRef.afterClosed().subscribe(confirma => {
       if (confirma) {
-        this.acaoCompetenciaService.excluir(acaoCompetencia.id).subscribe(() => {
-          this.acaoCompetencia.id = null
+        this.acoesCompetenciaService.excluir(acaoCompetencia.id).subscribe(() => {
+          this.acaoCompetencia.id = null;
           this.consultar();
         })
       } else {
@@ -106,16 +100,17 @@ export class AcaoCompetenciaComponent implements OnInit {
   }
 
   getAll() {
-    this.acaoCompetenciaService.getAll().subscribe((listaAcaoCompetencia: AcaoCompetencia[]) => {
-      this.listaAcaoCompetencia = listaAcaoCompetencia;
-      this.dataSource.data = listaAcaoCompetencia ? listaAcaoCompetencia : [];
-      this.verificaMostrarTabela(listaAcaoCompetencia);
+    this.acoesCompetenciaService.getAll().subscribe((acaoCompetencias: AcaoCompetencia[]) => {
+      this.listaAcaoCompetencia = acaoCompetencias;
+      this.dataSource.data = acaoCompetencias ? acaoCompetencias : [];
+      this.verificaMostrarTabela(acaoCompetencias);
     })
   }
+
   verificaMostrarTabela(acaoCompetencias: AcaoCompetencia[]) {
     if (!acaoCompetencias || acaoCompetencias.length == 0) {
       this.mostrarTabela = false;
-      this.msg = "Nenhuma Ação Competência cadastradas."
+      this.msg = "Nenhuma acaoCompetencia cadastrada."
     } else {
       this.mostrarTabela = true;
     }
