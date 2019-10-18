37d4cc2fa5f92eaa2c9cd2a9403681cfdd2181dd
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-10-18 20:46:59 -0300

    Atendimento

--- src/app/components/atendimento/cadastrar-atendimento/cadastrar-atendimento.component.ts
+++ src/app/components/atendimento/cadastrar-atendimento/cadastrar-atendimento.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Atendimento } from 'src/app/core/atendimento';
+import { Diagnostico } from 'src/app/core/diagnostico';
+import { PerfilAcesso } from 'src/app/core/perfil-acesso';
+import { AlunoService } from 'src/app/services/aluno/aluno.service';
+import { AtendimentosService } from 'src/app/services/atendimentos/atendimentos.service';
+import { DiagnosticoAtendimentoService } from 'src/app/services/diagnostico-atendimento/diagnostico-atendimento.service';
+import { ToastService } from 'src/app/services/toast/toast.service';
+import { Aluno } from './../../../core/aluno';
+import { Solucoes } from './../../../core/solucoes';
+import { SolucaoAtendimentoService } from './../../../services/solucao-atendimento/solucao-atendimento.service';
 
 @Component({
   selector: 'app-cadastrar-atendimento',
@@ -7,9 +18,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarAtendimentoComponent implements OnInit {
 
-  constructor() { }
+  diagnosticos: Diagnostico[];
+  solucoes: Solucoes[];
+  alunos: Aluno[];
+  atendimento: Atendimento = new Atendimento();
+
+
+  perfilAcesso: PerfilAcesso;
+  mostrarBotaoCadastrar = true
+  mostrarBotaoAtualizar = true;
+
+  isAtualizar: boolean = false;
+
+  constructor(
+    private atendimentoService: AtendimentosService,
+    private solucaoAtendimentoService: SolucaoAtendimentoService,
+    private diagnosticoAtendimentoService: DiagnosticoAtendimentoService,
+    private alunoService: AlunoService,
+    private activatedRoute: ActivatedRoute,
+    private toastService: ToastService,
+    private router: Router
+  ) {
+  }
+
 
   ngOnInit() {
+
+    this.atendimento.aluno = new Aluno();
+    this.atendimento.diagnostico = new Diagnostico();
+    this.atendimento.solucoes = new Solucoes();
+
+    this.perfilAcesso = this.activatedRoute.snapshot.data.perfilAcesso[0];
+
+    if (!this.perfilAcesso.insere) {
+      this.mostrarBotaoCadastrar = false;
+    }
+
+    if (!this.perfilAcesso.altera) {
+      this.mostrarBotaoAtualizar = false;
+    }
+    this.solucaoAtendimentoService.getAll().subscribe((solucoes: Solucoes[]) => {
+      this.solucoes = solucoes;
+    })
+
+    this.diagnosticoAtendimentoService.getAll().subscribe((diagnosticos: Diagnostico[]) => {
+      this.diagnosticos = diagnosticos;
+    })
+
+    this.alunoService.getAll().subscribe((alunos: Aluno[]) => {
+      this.alunos = alunos;
+    })
+
+    let idAtendimento: number;
+    idAtendimento = this.activatedRoute.snapshot.queryParams.idAtendimento ? this.activatedRoute.snapshot.queryParams.idAtendimento : null;
+    if (idAtendimento) {
+      this.isAtualizar = true;
+      this.atendimentoService.getById(idAtendimento).subscribe((atendimento: Atendimento) => {
+        this.atendimento = atendimento
+      });
+    }
+
+  }
+  mostrarBotaoLimpar() {
+    if (this.isAtualizar) return false;
+    if (!this.mostrarBotaoAtualizar) return false;
+    if (!this.mostrarBotaoCadastrar) return false;
+
+    return true;
+  }
+
+  cadastrar() {
+    this.atendimentoService.cadastrar(this.atendimento).subscribe(() => {
+      this.router.navigate(['atendimento']);
+      this.toastService.showSucesso("Atendimento cadastrado com sucesso");
+    });
+  }
+
+  limpar() {
+    this.atendimento = new Atendimento();
+  }
+
+  cancelar() {
+    this.router.navigate(['atendimento']);
+  }
+
+
+  atualizar() {
+    this.atendimentoService.alterar(this.atendimento).subscribe(() => {
+      this.router.navigate(['atendimento']);
+      this.toastService.showSucesso("Atendimento atualizado com sucesso");
+    });
+
   }
 
 }
