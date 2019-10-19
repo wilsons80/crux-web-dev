1e92e7364da8f4e85316ed86b35ea8ac62a5c49e
Josue Rodrigues <josuerodrigues@iMac-de-Josue.local>
2019-10-19 16:12:41 -0300

    participantes

--- src/app/components/participante-atendimento/participante-atendimento-routing.module.ts
+++ src/app/components/participante-atendimento/participante-atendimento-routing.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CadastrarParticipanteAtendimentoComponent } from './cadastrar-participante-atendimento/cadastrar-participante-atendimento.component';
 import { ParticipanteAtendimentoComponent } from './participante-atendimento.component';
+import { ParticipanteAtendimento } from 'src/app/core/participante-atendimento';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'src/app/guards/auth.guard';
+import { CadastrarParticipanteAtendimentoComponent } from './cadastrar-participante-atendimento/cadastrar-participante-atendimento.component';
 
 
 const routes: Routes = [
-  { path: 'participanteatendimento/cadastrar', component: CadastrarParticipanteAtendimentoComponent, canActivate: [AuthGuard]},
-  { path: 'participanteatendimento', component: ParticipanteAtendimentoComponent, canActivate: [AuthGuard]},
+  { path: 'participanteatendimento/cadastrar', component: CadastrarParticipanteAtendimentoComponent, canActivate: [AuthGuard] },
+  { path: 'participanteatendimento', component: ParticipanteAtendimentoComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
