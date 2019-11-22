527d762ec626a05e268df89efeb23a2bc9a0efa7
nrjosue <nrjosue@gmail.com>
2019-11-22 03:49:07 -0300

    programa

--- src/app/components/programas/cadastrar-programas/cadastrar-programas.component.ts
+++ src/app/components/programas/cadastrar-programas/cadastrar-programas.component.ts
@@ -1,17 +1,14 @@
-import { FuncionarioService } from './../../../services/funcionario/funcionario.service';
-import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Iniciativa } from 'src/app/core/iniciativa';
-import { Objetivo } from 'src/app/core/objetivo';
-import { IniciativaService } from 'src/app/services/iniciativa/iniciativa.service';
-import { ObjetivoService } from 'src/app/services/objetivo/objetivo.service';
-import { ProgramaService } from 'src/app/services/programa/programa.service';
-import { Programa } from './../../../core/programa';
-import { ToastService } from 'src/app/services/toast/toast.service';
+import * as _ from 'lodash';
 import { Acesso } from 'src/app/core/acesso';
 import { Funcionario } from 'src/app/core/funcionario';
-import * as _ from 'lodash';
+import { Objetivo } from 'src/app/core/objetivo';
+import { ObjetivoService } from 'src/app/services/objetivo/objetivo.service';
+import { ProgramaService } from 'src/app/services/programa/programa.service';
+import { ToastService } from 'src/app/services/toast/toast.service';
+import { Programa } from './../../../core/programa';
+import { FuncionarioService } from './../../../services/funcionario/funcionario.service';
 
 @Component({
   selector: 'app-cadastrar-programas',
@@ -20,7 +17,6 @@ import * as _ from 'lodash';
 })
 export class CadastrarProgramasComponent implements OnInit {
 
-  iniciativas: Iniciativa[];
   objetivos: Objetivo[];
   funcionarios: Funcionario[];
   programa: Programa;
@@ -32,16 +28,15 @@ export class CadastrarProgramasComponent implements OnInit {
   mostrarBotaoCadastrar = true
   mostrarBotaoAtualizar = true;
 
-  funcionario:Funcionario = new Funcionario();
+  funcionario: Funcionario = new Funcionario();
 
   constructor(
-    private iniciativaService: IniciativaService,
     private objetivoService: ObjetivoService,
     private programaService: ProgramaService,
     private funcionarioService: FuncionarioService,
     private activatedRoute: ActivatedRoute,
-    private router:Router,
-    private toastService:ToastService
+    private router: Router,
+    private toastService: ToastService
   ) {
   }
 
@@ -52,17 +47,14 @@ export class CadastrarProgramasComponent implements OnInit {
 
     this.perfilAcesso = this.activatedRoute.snapshot.data.perfilAcesso[0];
 
-    if(!this.perfilAcesso.insere){
+    if (!this.perfilAcesso.insere) {
       this.mostrarBotaoCadastrar = false;
     }
-    
-    if(!this.perfilAcesso.altera){
+
+    if (!this.perfilAcesso.altera) {
       this.mostrarBotaoAtualizar = false;
     }
-    this.iniciativaService.getAll().subscribe((iniciativas: Iniciativa[]) => {
-      this.iniciativas = iniciativas;
-    })
-   
+
     this.funcionarioService.getAll().subscribe((funcionarios: Funcionario[]) => {
       this.funcionarios = funcionarios;
     })
@@ -83,15 +75,14 @@ export class CadastrarProgramasComponent implements OnInit {
   }
   inicializarObjetos() {
     this.programa = new Programa();
-    this.programa.iniciativa = new Iniciativa();
     this.programa.objetivo = new Objetivo();
-    
+
   }
 
-  mostrarBotaoLimpar(){
-    if(this.isAtualizar) return false;
-    if(!this.mostrarBotaoAtualizar) return false;
-    if(!this.mostrarBotaoCadastrar) return false;
+  mostrarBotaoLimpar() {
+    if (this.isAtualizar) return false;
+    if (!this.mostrarBotaoAtualizar) return false;
+    if (!this.mostrarBotaoCadastrar) return false;
 
     return true;
   }
@@ -103,7 +94,7 @@ export class CadastrarProgramasComponent implements OnInit {
   }
 
   limpar() {
-   this.inicializarObjetos();
+    this.inicializarObjetos();
   }
 
   cancelar() {
@@ -119,8 +110,8 @@ export class CadastrarProgramasComponent implements OnInit {
 
   }
 
-  mostrarDadosFuncionario(idFuncionario:number) {
-     this.funcionario = _.cloneDeep(_.find(this.funcionarios, (f: Funcionario) => f.id === idFuncionario));
+  mostrarDadosFuncionario(idFuncionario: number) {
+    this.funcionario = _.cloneDeep(_.find(this.funcionarios, (f: Funcionario) => f.id === idFuncionario));
   }
 
 }
--- src/app/core/programa.ts
+++ src/app/core/programa.ts
@@ -1,16 +1,14 @@
-import { Iniciativa } from './iniciativa';
 import { Objetivo } from './objetivo';
 
-export class Programa{
-    id:number;
-	nome:string;
-	descricao:string;
-	dataInicio:Date;
-	dataFim:Date;
-	idCoordenador:number;
-	faixaEtariaFim:number;
-	faixaEtariaInicio:number;
-	objetivo:Objetivo;
-	iniciativa:Iniciativa;
-	usuarioAlteracao:number;
+export class Programa {
+	id: number;
+	nome: string;
+	descricao: string;
+	dataInicio: Date;
+	dataFim: Date;
+	idCoordenador: number;
+	faixaEtariaFim: number;
+	faixaEtariaInicio: number;
+	objetivo: Objetivo;
+	usuarioAlteracao: number;
 }
