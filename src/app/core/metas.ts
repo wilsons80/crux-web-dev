1798110eec6c4610cd6ce4b991c7081e2fd0028d
nrjosue <nrjosue@gmail.com>
2019-11-28 01:10:45 -0300

    metas

--- src/app/core/metas.ts
+++ src/app/core/metas.ts
@@ -3,6 +3,7 @@ import { Indicadores } from './indicadores';
 export class Metas{
     id: number;
 	nome:string;
+	descricao:string;
 	indicadores: Indicadores;
 	usuarioAlteracao:number;
 	dataFim:Date;
