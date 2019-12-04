41e183ba4bc58362ce66255b666af392389c023a
Wilson Souza <wilsons80@gmail.com>
2019-12-04 19:09:58 -0300

    Ajuste no modulo de turmas

--- src/app/components/turmas/turmas.module.ts
+++ src/app/components/turmas/turmas.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TurmasRoutingModule } from './turmas-routing.module';
 import { CadastrarTurmasComponent } from './cadastrar-turmas/cadastrar-turmas.component';
 import { MaterialCommonModule } from 'src/app/material-modules/material-common.module';
+import { TurmasComponent } from './turmas.component';
 
 @NgModule({
-  declarations: [CadastrarTurmasComponent],
+  declarations: [TurmasComponent, CadastrarTurmasComponent],
   imports: [
     CommonModule,
     TurmasRoutingModule,
